4228ebc7fa2dcdc41962bd3f6c930b5cf581a0b4
Gabor Schmied <gabor.schmied@cognex.com>
2017-09-20 16:54:39 +0200

    Add project files.

---
+++ SignalRManagedClient/HubSample.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.Sockets.Client;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+
+namespace ClientSample
+{
+	internal class HubSample
+	{
+		internal static void Register(CommandLineApplication app)
+		{
+			app.Command("hub", cmd =>
+			{
+				cmd.Description = "Tests a connection to a hub";
+
+				var baseUrlArgument = cmd.Argument("<BASEURL>", "The URL to the Chat Hub to test");
+
+				cmd.OnExecute(() => ExecuteAsync(baseUrlArgument.Value));
+			});
+		}
+
+		public static async Task<int> ExecuteAsync(string baseUrl)
+		{
+			baseUrl = string.IsNullOrEmpty(baseUrl) ? "http://localhost:5000/hubs" : baseUrl;
+
+			var loggerFactory = new LoggerFactory();
+
+			Console.WriteLine("Connecting to {0}", baseUrl);
+			var httpConnection = new HttpConnection(new Uri(baseUrl));
+			var connection = new HubConnection(httpConnection, loggerFactory);
+			try
+			{
+				await connection.StartAsync();
+				Console.WriteLine("Connected to {0}", baseUrl);
+
+				var cts = new CancellationTokenSource();
+				Console.CancelKeyPress += (sender, a) =>
+				{
+					a.Cancel = true;
+					Console.WriteLine("Stopping loops...");
+					cts.Cancel();
+				};
+
+				// Set up handler
+				connection.On<string>("Send", Console.WriteLine);
+
+				while (!cts.Token.IsCancellationRequested)
+				{
+					var line = await Task.Run(() => Console.ReadLine(), cts.Token);
+
+					if (line == null)
+					{
+						break;
+					}
+
+					await connection.Invoke<object>("Send", cts.Token, line);
+				}
+			}
+			catch (AggregateException aex) when (aex.InnerExceptions.All(e => e is OperationCanceledException))
+			{
+			}
+			catch (OperationCanceledException)
+			{
+			}
+			finally
+			{
+				await connection.DisposeAsync();
+			}
+			return 0;
+		}
+	}
+}
---
+++ SignalRManagedClient/RawSample.cs
@@ -0,0 +1,96 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Sockets.Client;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ClientSample
+{
+	internal class RawSample
+	{
+		internal static void Register(CommandLineApplication app)
+		{
+			app.Command("raw", cmd =>
+			{
+				cmd.Description = "Tests a connection to an endpoint";
+
+				var baseUrlArgument = cmd.Argument("<BASEURL>", "The URL to the Chat EndPoint to test");
+
+				cmd.OnExecute(() => ExecuteAsync(baseUrlArgument.Value));
+			});
+		}
+
+		public static async Task<int> ExecuteAsync(string baseUrl)
+		{
+			baseUrl = string.IsNullOrEmpty(baseUrl) ? "http://localhost:53042/chat" : baseUrl;
+
+			var loggerFactory = new LoggerFactory();
+			var logger = loggerFactory.CreateLogger<Program>();
+
+			Console.WriteLine($"Connecting to {baseUrl}...");
+
+			var connection = new HubConnectionBuilder()
+				.WithUrl(baseUrl)
+				.WithConsoleLogger()
+				.Build();
+
+			//var connection = new HttpConnection(new Uri(baseUrl), loggerFactory);
+			try
+			{
+				var cts = new CancellationTokenSource();
+				connection.On<string>("Send", Connection_MessageReceived);
+				connection.Closed += e =>
+				{
+					return Task.CompletedTask;
+				};
+
+				await connection.StartAsync();
+
+				Console.WriteLine($"Connected to {baseUrl}");
+
+				Console.CancelKeyPress += (sender, a) =>
+				{
+					a.Cancel = true;
+					cts.Cancel();
+				};
+
+				while (!cts.Token.IsCancellationRequested)
+				{
+					var line = await Task.Run(() => Console.ReadLine(), cts.Token);
+
+					if (line == null)
+					{
+						break;
+					}
+
+					await connection.SendAsync("Send", cts.Token, line);
+				}
+			}
+			catch (AggregateException aex) when (aex.InnerExceptions.All(e => e is OperationCanceledException))
+			{
+			}
+			catch (OperationCanceledException)
+			{
+			}
+			finally
+			{
+				await connection.DisposeAsync();
+			}
+			return 0;
+		}
+
+		private static async void Connection_MessageReceived(string message)
+		{
+			await Console.Out.WriteLineAsync($"{message}");
+		}
+	}
+}
